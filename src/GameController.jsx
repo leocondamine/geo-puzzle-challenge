0d7680eb469b4a11525d58f827006bd746dd65d1
leocondamine <118886247+leocondamine@users.noreply.github.com>
2023-04-05 19:30:02 +0200

    fetch working

--- src/GameController.jsx
+++ src/GameController.jsx
@@ -3,12 +3,35 @@ import MapDisplay from "./components/MapDisplay";
 
 const GameController = () => {
   const [selectedFeature, setSelectedFeature] = useState([]);
-  const [featuresToGuess, setFeaturesToGuess] = useState([
-    "France",
-    "Italy",
-    "Spain",
-  ]);
-  const [guess, setGuess] = useState(featuresToGuess[0]);
+  const [featuresToGuess, setFeaturesToGuess] = useState([]);
+  const [guess, setGuess] = useState([]);
+
+  // const initFeaturesToGuess = () => {
+
+  //       setFeaturesToGuess(names);
+  //       setGuess(names[0]);
+  //       return;
+  //     })
+
+  // };
+
+  useEffect(() => {
+    const fetchFeatures = async () => {
+      fetch(
+        "https://cdn.rawgit.com/nvkelso/natural-earth-vector/master/geojson/ne_10m_admin_0_countries.geojson"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const names = data.features.map((feature) => feature.properties.NAME);
+          console.log(names[0]);
+          setFeaturesToGuess(names);
+          setGuess(names[0]);
+        })
+        .catch((error) => console.error(error));
+    };
+
+    fetchFeatures();
+  }, []);
 
   const handleMapData = (data) => {
     setSelectedFeature(data);
@@ -24,16 +47,19 @@ const GameController = () => {
   }, [selectedFeature]);
 
   const setNewGuess = () => {
-    setFeaturesToGuess(featuresToGuess.slice(1));
-    console.log(featuresToGuess);
-    if (featuresToGuess.length > 1) {
-      setGuess(featuresToGuess[1]);
-      console.log(guess);
+    const remainingFeaturesToGuess = featuresToGuess.slice(1)
+    const newFeatureToGuess = remainingFeaturesToGuess[0]
+    setFeaturesToGuess(remainingFeaturesToGuess);
+    console.log(remainingFeaturesToGuess)
+    console.log("new Feature :");
+    console.log(newFeatureToGuess);
+    if (featuresToGuess.length > 0) {
+      setGuess(newFeatureToGuess);
     } else {
       setGameIsFinished();
     }
   };
-  // useEffect(() => {console.log(guess)},[guess]); // DEBUG
+
 
   const setGameIsFinished = () => {
     console.log("the game is finished");
